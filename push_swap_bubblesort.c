a8f3abf0497fc6ed3d68bdb8207b9e9a2c6e6d95
DarkNomadus <sscheini@c2r5s2.42malaga.com>
2025-03-25 13:29:08 +0100

    | Update March 25 - Norminette corrected |

--- push_swap_bubblesort.c
+++ push_swap_bubblesort.c
@@ -6,7 +6,7 @@
 /*   By: sscheini <sscheini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 17:45:28 by sscheini          #+#    #+#             */
-/*   Updated: 2025/03/20 18:42:14 by sscheini         ###   ########.fr       */
+/*   Updated: 2025/03/25 13:22:59 by sscheini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,8 +76,8 @@ static int	ft_minmax_orders(t_list *stack)
 
 static void	ft_solve(t_list **stacks, char **order_lst)
 {
-	t_pair order;
-	
+	t_pair	order;
+
 	order.a = ft_minmax_orders(stacks[0]);
 	order.b = ft_translate(ft_maxmin_orders(stacks[1]));
 	if (order.a == order.b - 3)
@@ -91,7 +91,7 @@ static void	ft_solve(t_list **stacks, char **order_lst)
 		ft_printf("%s\n", order_lst[order.b]);
 }
 
-/*	Executes instructions to sort an individual stack of a numeric 			*/ 
+/*	Executes instructions to sort an individual stack of a numeric 			*/
 /*	T_LIST ** with an order efficiency of: O(n).							*/
 /*	NOTICE 																	*/
 /*	|-|																		*/
@@ -111,7 +111,7 @@ void	ft_bubblesort(t_list **stacks, char **order_lst)
 			if (ft_execute(PB_ORDER, stacks))
 				ft_printf("%s\n", order_lst[PB_ORDER]);
 		}
-		while (ft_checksort_lst(stacks[0], -1) 
+		while (ft_checksort_lst(stacks[0], -1)
 			|| ft_checksort_lst(stacks[1], 1))
 			ft_solve(stacks, order_lst);
 		run.a = 0;
